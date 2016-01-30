59e6b34f9bb09f35b5f58b1c5a754ca9d72f45fe
deanboole <xspiritualx@gmail.com>
2016-01-30 16:35:22 +0800

    add example for using commands.py

---
+++ cmd-example.py
@@ -0,0 +1,11 @@
+# import os
+# import glob
+# import atexit
+# import readline
+#
+# from konsole.session import __session__
+from konsole.commands import Commands
+
+cmd = Commands()
+
+cmd.commands['help']['obj']()
