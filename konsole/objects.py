d3cc5dcfebcbde7bf4762c27b828e4324e0d1de3
deanboole <xspiritualx@gmail.com>
2016-02-03 14:55:52 +0800

    add codes in object for making database works

--- konsole/objects.py
+++ konsole/objects.py
@@ -8,6 +8,13 @@ try:
 except ImportError:
     HAVE_SSDEEP = False
 
+class Singleton(type):
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
 class File(object):
 
     def __init__(self, path):
