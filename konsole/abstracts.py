64e25f48ea02a6837aa09039ce8d161f5f8add47
deanboole <xspiritualx@gmail.com>
2016-02-03 14:56:33 +0800

    add example for showing modules

---
+++ konsole/abstracts.py
@@ -0,0 +1,18 @@
+from konsole.colors import *
+
+class Module(object):
+    cmd = ''
+    description = ''
+    args = []
+
+    def set_args(self, args):
+        self.args = args
+
+    def usage(self):
+        raise NotImplementedError
+
+    def help(self):
+        raise NotImplementedError
+
+    def run(self, *args):
+        raise NotImplementedError
