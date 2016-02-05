fbb226469da19dec62207fca21c37a43640a3542
deanboole <xspiritualx@gmail.com>
2016-02-05 23:26:29 +0800

    add example for executing os commands

--- code-snippets/startwith-example.py
+++ code-snippets/startwith-example.py
@@ -1,4 +1,10 @@
-cmd = "!123"
+import os
 
-if cmd.startswith('!'):
+cmd1 = "!123"
+cmd2 = "!date"
+
+if cmd1.startswith('!'):
     print "gotcha"
+
+if cmd2.startswith('!'):
+    os.system(cmd2[1:])
