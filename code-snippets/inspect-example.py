e8b3586176319432ad0442b3ca5f27ae06ecc723
deanboole <xspiritualx@gmail.com>
2016-02-06 11:57:49 +0800

    add example for using module inspect

---
+++ code-snippets/inspect-example.py
@@ -0,0 +1,20 @@
+import pkgutil
+import inspect
+
+# this is the package we are inspecting -- for example 'email' from stdlib
+import modules
+
+package = modules
+prefix = package.__name__ + "."
+
+for importer, modname, ispkg in pkgutil.walk_packages(package.__path__, prefix):
+    print "Found submodule %s (is a package: %s)" % (modname, ispkg)
+
+    module = __import__(modname, fromlist="dummy")
+    print "Imported", module
+
+for member_name, member_object in inspect.getmembers(module):
+    if inspect.isclass(member_object):
+        print "%s is class" % (member_object)
+
+# http://stackoverflow.com/questions/1707709/list-all-the-modules-that-are-part-of-a-python-package
